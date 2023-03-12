40b1437948bf6554d295f75c32b518456deebe72
mrinalwahal <mrinalwahal@gmail.com>
2023-03-13 00:11:19 +0530

    Brew formula update for envsecrets version v0.0.8

--- envsecrets.rb
+++ envsecrets.rb
@@ -5,11 +5,11 @@
 class Envsecrets < Formula
   desc "CLI-first management of your environment secrets."
   homepage "https://envsecrets.com"
-  version "0.0.7"
+  version "0.0.8"
 
   on_macos do
-    url "https://github.com/envsecrets/envsecrets/releases/download/v0.0.7/envsecrets_0.0.7_darwin_all.tar.gz"
-    sha256 "d4e690c0beff4e38e9408abdd39529d23620e3b060c763c67683dfeb6de4ad8f"
+    url "https://github.com/envsecrets/envsecrets/releases/download/v0.0.8/envsecrets_0.0.8_darwin_all.tar.gz"
+    sha256 "21d8f57f0f681319b1a607af83ad7b725cd4467af8feb5a874f5248b7aa39a05"
 
     def install
       bin.install "envsecrets"
@@ -18,16 +18,16 @@ class Envsecrets < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/envsecrets/envsecrets/releases/download/v0.0.7/envsecrets_0.0.7_linux_arm64.tar.gz"
-      sha256 "f571cca7176761b7221db584e4c5b466dd5407f13a45fb5971b4cf60c9a55203"
+      url "https://github.com/envsecrets/envsecrets/releases/download/v0.0.8/envsecrets_0.0.8_linux_arm64.tar.gz"
+      sha256 "a31b48224f01180d9ea84b753621f6a33c5d687929fee4ba304e67e7ba39e219"
 
       def install
         bin.install "envsecrets"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/envsecrets/envsecrets/releases/download/v0.0.7/envsecrets_0.0.7_linux_amd64.tar.gz"
-      sha256 "831478b9d57d768d36beb1b3bad63d554fef81a32b1322c72e476d6235e6226d"
+      url "https://github.com/envsecrets/envsecrets/releases/download/v0.0.8/envsecrets_0.0.8_linux_amd64.tar.gz"
+      sha256 "637fde6330ba5cc9556aba224241c8628b87d17cc0991ff97089357ceec10e5f"
 
       def install
         bin.install "envsecrets"
